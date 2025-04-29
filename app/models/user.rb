c00c934ce41ec16f9d8c593964bf9034be93b131
Tabasa-T <t6224946t@gmail.com>
2025-04-29 16:05:36 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname, length: {maximum: 15}
+    validates :password, format: { 
+                          with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, 
+                          message: 'は半角英数字混合で入力してください' 
+                         }, on: :create
+  end
 end
