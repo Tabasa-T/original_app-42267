c491ade57b0fb02fc7161bcf3ac808446a5496de
Tabasa-T <t6224946t@gmail.com>
2025-05-03 01:00:47 +0900

    createアクションの定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_post, only: [:show, :edit, :update]
+  
   def index
     @posts = Post.all.order('created_at DESC')
   end
@@ -17,16 +19,28 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+
   end
 
   def edit
-    @post = Post.find(params[:id])
+
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
 
   def post_params
     params.require(:post).permit(:duration, :result, :image).merge(user_id: current_user.id)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
