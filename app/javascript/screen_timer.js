ca9d1a1c6d0b2b952dc3e97cdb8e6824b1a7e375
Tabasa-T <t6224946t@gmail.com>
2025-05-04 20:15:36 +0900

    指定時間経過でアラート表示

---
+++ app/javascript/screen_timer.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const LIMIT_TIME = 5 * 60 * 1000; // 3分（ミリ秒）
+
+  setTimeout(() => {
+    alert("あなたの今日の価値作りはここからだね！少し休憩して、また来てね☕️");
+  }, LIMIT_TIME);
+});
