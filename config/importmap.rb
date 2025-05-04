a0725f8578571a6039a57baf4313b47165486345
Tabasa-T <t6224946t@gmail.com>
2025-05-04 15:41:33 +0900

    JavaScript読み込み設定

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "chartkick", to: "chartkick.js"
+pin "chart.js", to: "https://cdn.jsdelivr.net/npm/chart.js@4.4.9/dist/chart.umd.js"
+pin "chartjs-adapter-date-fns", to: "https://cdn.jsdelivr.net/npm/chartjs-adapter-date-fns@3.0.0/dist/chartjs-adapter-date-fns.bundle.min.js"
+
