a9c60126666cbd36cffc941e69c1bfa9d1088254
Tabasa-T <t6224946t@gmail.com>
2025-05-07 02:04:11 +0900

    タイムゾーン設定変更

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module OriginalApp42267
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Tokyo'
   end
 end
