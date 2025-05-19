fd1c8f949c1b3747fc0e80a864d0663a1e4d7145
Tabasa-T <t6224946t@gmail.com>
2025-05-19 23:56:43 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root "home#index"
-  resources :posts
+  resources :posts do
+    resource :like, only: [:create, :destroy]
+  end
   get 'mypage', to: 'posts#mypage', as: 'mypage'
 end
