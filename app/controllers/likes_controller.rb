fb3ffbaafcd641a99e27848a43da1c66ab4ac19e
Tabasa-T <t6224946t@gmail.com>
2025-05-21 23:19:01 +0900

    アクションの定義

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,24 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_post
+
+  def create
+    post = Post.find(params[:post_id])
+    post.likes.create(user: current_user)
+    redirect_back fallback_location: post_path
+  end
+
+  def destroy
+    post = Post.find(params[:post_id])
+    like = post.likes.find_by(user: current_user)
+    like.destroy if like
+    redirect_back fallback_location: posts_path
+  end
+
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
 end
