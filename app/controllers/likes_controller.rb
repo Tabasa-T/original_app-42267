ecf71664ddac0b118372ac67054b555731145a7a
Tabasa-T <t6224946t@gmail.com>
2025-05-25 23:58:26 +0900

    コントローラー修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,14 +5,14 @@ class LikesController < ApplicationController
   def create
     post = Post.find(params[:post_id])
     post.likes.create(user: current_user)
-    redirect_back fallback_location: post_path
+    redirect_back fallback_location: post_path(post)
   end
 
   def destroy
     post = Post.find(params[:post_id])
     like = post.likes.find_by(user: current_user)
     like.destroy if like
-    redirect_back fallback_location: posts_path
+    redirect_back fallback_location: posts_path(post)
   end
 
 
