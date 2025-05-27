29bdfe95f74a997e1290c5084f2b93314aa9f0f0
Tabasa-T <t6224946t@gmail.com>
2025-05-28 01:01:12 +0900

    indexアクション設定

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,18 +1,20 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_post
+  before_action :set_post, only: [:create, :destroy]
 
   def create
-    post = Post.find(params[:post_id])
-    post.likes.create(user: current_user)
-    redirect_back fallback_location: post_path(post)
+    @post.likes.create(user: current_user)
+    redirect_back fallback_location: post_path(@post)
   end
 
   def destroy
-    post = Post.find(params[:post_id])
-    like = post.likes.find_by(user: current_user)
+    like = @post.likes.find_by(user: current_user)
     like.destroy if like
-    redirect_back fallback_location: posts_path(post)
+    redirect_back fallback_location: posts_path(@post)
+  end
+
+  def index
+    @liked_posts = current_user.liked_posts.order(created_at: :desc)
   end
 
 
